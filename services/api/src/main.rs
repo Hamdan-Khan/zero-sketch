3a40eef2d861926a99aac1ab2c12bd42e16131d7
Hamdan-Khan <hamdankhan212@gmail.com>
2026-08-12 18:06:35 +0500

    add middleware tracing

--- services/api/src/main.rs
+++ services/api/src/main.rs
@@ -1,18 +1,55 @@
-use axum::{Router, response::Html, routing::get};
+use axum::{
+    BoxError, Json, Router, error_handling::HandleErrorLayer, http::StatusCode, routing::get,
+};
+use dotenvy::dotenv;
+use serde::Serialize;
+use std::time::Duration;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    dotenv().ok();
 
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let trace_layer = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(|error: BoxError| async move {
+            if error.is::<tower::timeout::error::Elapsed>() {
+                Ok(StatusCode::REQUEST_TIMEOUT)
+            } else {
+                Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {error}"),
+                ))
+            }
+        }))
+        .timeout(Duration::from_secs(10))
+        .layer(TraceLayer::new_for_http())
+        .into_inner();
+
+    let app = Router::new().route("/", get(handler)).layer(trace_layer);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await
         .unwrap();
+
     println!("listening on {}", listener.local_addr().unwrap());
     let _ = axum::serve(listener, app).await;
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+#[derive(Serialize)]
+struct ApiResponse {
+    message: String,
+    success: bool,
+}
+
+async fn handler() -> Json<ApiResponse> {
+    Json(ApiResponse {
+        message: "Hello from ZeroSketch!".to_string(),
+        success: true,
+    })
 }
