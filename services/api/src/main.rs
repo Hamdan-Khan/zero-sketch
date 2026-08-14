54d11ede72153688896944d4a0f8fa0a29d2fdc8
Hamdan-Khan <hamdankhan212@gmail.com>
2026-08-14 17:58:37 +0500

    add diagram upload endpoint

--- services/api/src/main.rs
+++ services/api/src/main.rs
@@ -1,12 +1,24 @@
+use aws_config::BehaviorVersion;
+use aws_sdk_s3::{self as r2, Client};
 use axum::{
-    BoxError, Json, Router, error_handling::HandleErrorLayer, http::StatusCode, routing::get,
+    BoxError, Json, Router,
+    error_handling::HandleErrorLayer,
+    extract::{Multipart, State},
+    http::StatusCode,
+    routing::{get, post},
 };
 use dotenvy::dotenv;
 use serde::Serialize;
-use std::time::Duration;
+use std::{env, time::Duration};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::EnvFilter;
+
+#[derive(Clone)]
+struct AppState {
+    r2_client: Client,
+    diagrams_bucket_name: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -16,6 +28,7 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
+    // logging middleware
     let trace_layer = ServiceBuilder::new()
         .layer(HandleErrorLayer::new(|error: BoxError| async move {
             if error.is::<tower::timeout::error::Elapsed>() {
@@ -31,12 +44,47 @@ async fn main() {
         .layer(TraceLayer::new_for_http())
         .into_inner();
 
-    let app = Router::new().route("/", get(handler)).layer(trace_layer);
+    let r2_client = create_r2_client().await;
+    let diagrams_bucket_name =
+        env::var("R2_DIAGRAMS_BUCKET_NAME").expect("R2_DIAGRAMS_BUCKET_NAME must be set");
+
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/upload", post(upload_diagram))
+        .layer(trace_layer)
+        .with_state(AppState {
+            r2_client,
+            diagrams_bucket_name,
+        });
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
 
     println!("listening on {}", listener.local_addr().unwrap());
     let _ = axum::serve(listener, app).await;
+}
+
+async fn create_r2_client() -> Client {
+    let account_id = env::var("CLOUDFLARE_ACCOUNT_ID").expect("CLOUDFLARE_ACCOUNT_ID must be set");
+    let access_key_id = env::var("R2_ACCESS_KEY_ID").expect("R2_ACCESS_KEY_ID must be set");
+    let access_key_secret =
+        env::var("R2_ACCESS_KEY_SECRET").expect("R2_ACCESS_KEY_SECRET must be set");
+
+    // configure the client
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .endpoint_url(format!("https://{}.r2.cloudflarestorage.com", account_id))
+        .credentials_provider(r2::config::Credentials::new(
+            access_key_id,
+            access_key_secret,
+            None, // session token is not used with R2
+            None,
+            "R2",
+        ))
+        .region(r2::config::Region::new("auto")) // required by SDK but not used by R2
+        .load()
+        .await;
+
+    // Create R2 client
+    r2::Client::new(&config)
 }
 
 #[derive(Serialize)]
@@ -50,4 +98,37 @@ async fn handler() -> Json<ApiResponse> {
         message: "Hello from ZeroSketch!".to_string(),
         success: true,
     })
+}
+
+async fn upload_diagram(
+    State(state): State<AppState>,
+    mut multipart: Multipart,
+) -> Result<Json<ApiResponse>, (StatusCode, String)> {
+    let mut bytes = Vec::<u8>::new();
+    // collect bytes stream from multipart form data
+    while let Ok(Some(field)) = multipart.next_field().await {
+        let data = field
+            .bytes()
+            .await
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+        bytes.extend(data);
+    }
+    tracing::debug!("collected bytes: {:#?}", bytes);
+
+    tracing::info!("file size in bytes: {}", bytes.len());
+
+    state
+        .r2_client
+        .put_object()
+        .bucket(state.diagrams_bucket_name)
+        .key("test.txt")
+        .body(r2::primitives::ByteStream::from(bytes))
+        .send()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    Ok(Json(ApiResponse {
+        message: "Diagram uploaded successfully!".to_string(),
+        success: true,
+    }))
 }
